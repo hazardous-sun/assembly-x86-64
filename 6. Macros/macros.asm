0f17794468877ef73139b4a74e6095421379ba86
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-09 10:36:07 -0300

    added use of 'EQU' for defining constants

--- 6. Macros/macros.asm
+++ 6. Macros/macros.asm
@@ -1,3 +1,11 @@
+STDIN equ 0
+STDOUT equ 1
+STDERR equ 2
+
+SYS_READ equ 0
+SYS_WRITE equ 1
+SYS_EXIT equ 60
+
 section _data
     text db "Hello, World!", 10
 section .text
@@ -5,15 +13,15 @@ section .text
 
 ; Macro that calls the sys_write function to print the value of text to the standard output
 %macro printHelloWorld 0
-    mov rax, 1
-    mov rdi, 1
+    mov rax, SYS_WRITE
+    mov rdi, STDOUT
     mov rsi, text
     mov rdx, 14
     syscall
 %endmacro
 
 %macro exit 0
-    mov rax, 60
+    mov rax, SYS_EXIT
     mov rdi, 0
     syscall
 %endmacro
