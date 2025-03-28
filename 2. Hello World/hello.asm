b7152943b25766cdc9326c481d0110922583cf90
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-28 16:51:10 -0300

    removed invalid multiline comment style

--- 2. Hello World/hello.asm
+++ 2. Hello World/hello.asm
@@ -1,50 +1,41 @@
-/*
-All x86_64 assembly files have three sections, the ".data" section, the ".bss" section, 
-and the ".text" section.
-
-  1. The ".data" is the section of all the (guess what) DATA of the application is 
-     defined befor compilation.
-  2. The ".bss" section is where data is allocated for future use
-  3. The ".text" section is where the actual code goes.
-*/
+; All x86_64 assembly files have three sections, the ".data" section, the ".bss" section, 
+; and the ".text" section.
+; 1. The ".data" is the section of all the (guess what) DATA of the application is 
+;     defined befor compilation.
+; 2. The ".bss" section is where data is allocated for future use
+; 3. The ".text" section is where the actual code goes.
 section .data
-        /*
-        The word "db" stands for "define bytes". It essentially means that wea are going 
-        to define some raw bytes data to insert into our code.
-        
-        The data we are defining is "Hello, World!" plus a control line feed character 
-        to insert a new line. Each char is a single byte.
-
-        "text" is the name assigned to the address in memory that will contain the data. 
-        Whenever we use "text" later in the code, when the code is compiled, the compiler 
-        will determine the actual location in memory of this data and replace all the future
-        instances of "text" with that memory address.
-        */
+        ; The word "db" stands for "define bytes". It essentially means that wea are going 
+        ; to define some raw bytes data to insert into our code.
+        ;
+        ; The data we are defining is "Hello, World!" plus a control line feed character 
+        ; to insert a new line. Each char is a single byte.
+        ;
+        ; "text" is the name assigned to the address in memory that will contain the data. 
+        ; Whenever we use "text" later in the code, when the code is compiled, the compiler 
+        ; will determine the actual location in memory of this data and replace all the future
+        ; instances of "text" with that memory address.
         text db "Hello, World!", 10 ; passes "Hello, World!" plus the LF control char to a space of memory and sets 'text' as the reference to it
 
 section .text
-        /*
-        The word "global" is used when you want the linker to be able to know the address of some label.
-
-        The object file generated will contain a link to every label declared "global".
-
-        In this case, we have to declare "_start" as global, since it is required for the code to be properly linked.
-        */
+        ; The word "global" is used when you want the linker to be able to know the address of some label.
+        ;
+        ; The object file generated will contain a link to every label declared "global".
+        ;
+        ; In this case, we have to declare "_start" as global, since it is required for the code to be properly linked.
         global _start
 
-/*
-Assembly files also support "labels". A label is used to (guess one more time) LABEL a part of the code.
-
-Upon compilation, the compiler will calculate the location in which the label sit in memory.
-
-Any time the name of the label is used afterwards, that name is replaced by the location in memory by the compiler.
-
-The "_start" label is the entry part of the code and is essential for all Assembly programs.
-
-When the code is compiled and later executed, it is executed first at the location of "_start".
-
-If the linker cannot find "_start", it will throw an error.
-*/
+; Assembly files also support "labels". A label is used to (guess one more time) LABEL a part of the code.
+;
+; Upon compilation, the compiler will calculate the location in which the label sit in memory.
+;
+; Any time the name of the label is used afterwards, that name is replaced by the location in memory by the compiler.
+;
+; The "_start" label is the entry part of the code and is essential for all Assembly programs.
+;
+; When the code is compiled and later executed, it is executed first at the location of "_start".
+;
+; If the linker cannot find "_start", it will throw an error.
 _start:
         mov rax, 1    ; rax register informs which syscall to use, in this case, sys_write
         mov rdi, 1    ; rdi register informs which file descriptor to use, in this case, stdout
