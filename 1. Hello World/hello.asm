908bc411d64ab5f03adfc513d4755e1005aa2db6
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-28 16:51:09 -0300

    Added some comments to hello.asm informing what each line of the '_start:' section does.

--- 1. Hello World/hello.asm
+++ 1. Hello World/hello.asm
@@ -6,13 +6,13 @@ section .text
         global _start
 
 _start:
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, text
-        mov rdx, 14
-        syscall
+        mov rax, 1    ; rax register informs which syscall to use, in this case, sys_write
+        mov rdi, 1    ; rdi register informs which file descriptor to use, in this case, stdout
+        mov rsi, text ; rsi register informs which buffer to use, in this case, the 'text' memory address
+        mov rdx, 14   ; rdx register informs the size of the buffer, in the case of "Hello, World!" + the LF control char, 14
+        syscall       ; finishes the descrition for the syscall 
 
-        mov rax, 60
-        mov rdi, 0
+        mov rax, 60   ; rax register once again informs which syscall to use, in this case, sys_exit
+        mov rdi, 0    ; 
         syscall
 
